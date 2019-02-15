efd7c3a62d5d52ddb64d1d68f4c96e38ad54dedd
KJurewicz <k.jurewicz@nencki.gov.pl>
2019-02-15 11:09:41 +0100

    half of the function

---
+++ maxima.py
@@ -0,0 +1,9 @@
+def find_maxima(arr):
+	""" Returns the position of the maximum values """
+	for ind in arr:
+		index = 0
+		for i in range(1,len(arr)):
+			if arr[i] > arr[index]
+				index = i 
+		print index
+	return [2]
